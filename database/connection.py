2a9c3ea8437ea77bc9d107fd583154c2cc1d738c
monello <monello@monello.me>
2023-06-18 14:37:01 +0200

    dockerized app

--- database/connection.py
+++ database/connection.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:midnight@127.0.0.1/midnight"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:midnight@database/midnight"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
---
+++ docker/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+../environment/bin/python ../app.py
