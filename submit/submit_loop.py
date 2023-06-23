14e13fe10e707286aa2a131dfaac1aad4f5a4a3a
monello <monello@monello.me>
2023-06-23 20:55:51 +0200

    fixes

--- submit/submit_loop.py
+++ submit/submit_loop.py
@@ -14,16 +14,18 @@ def submit_loop():
     config = get_config()
 
     while True:
-        flags = 1connection.execute(text("SELECT * FROM flags WHERE status = 0 LIMIT 100")).fetchall()
+        flags = connection.execute(text("SELECT * FROM flags WHERE status = 0 LIMIT 100")).fetchall()
         flags = [Flag.from_orm(flag) for flag in flags]
 
         try:
+            time.sleep(10)
             response = requests.put(config['CHECKSYSTEM_URL'], headers={'X-Team-Token': config['CHECKSYSTEM_TOKEN']}, json=[item.flag for item in flags], timeout=5).json()
 
+            #print(response)
+
             for flag in response:
-                connection.execute(text("UPDATE flags SET status = :status, checksystem_response = :response WHERE flag = :flag"), {"status": FlagStatus.ACCEPTED.value if flag.status else FlagStatus.REJECTED.value, "flag": flag.flag, "response": flag.msg})
+                connection.execute(text("UPDATE flags SET status = :status, checksystem_response = :response WHERE flag = :flag"), {"status": FlagStatus.ACCEPTED.value if flag["status"] else FlagStatus.REJECTED.value, "flag": flag["flag"], "response": flag["msg"]})
 
             connection.commit()
-            time.sleep(10)
         except:
             pass
