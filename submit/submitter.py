f744fad414694323e0fe060665ad904403d92487
monello <monello@monello.me>
2023-06-27 17:38:37 +0200

    dockerfile fixes

--- submit/submitter.py
+++ submit/submitter.py
@@ -7,28 +7,29 @@ from config.config import get_config
 
 import requests
 import time
-import random
 
 def submit_loop():
     connection = next(get_db())
     config = get_config()
 
     while True:
-        flags = connection.execute(text("SELECT * FROM flags WHERE status = 0")).fetchall()
+        flags = connection.execute(text("SELECT * FROM flags WHERE status = 0 LIMIT 100")).fetchall()
         flags = [Flag.from_orm(flag) for flag in flags]
 
+        print(flags)
+
         try:
-            time.sleep(10)
+            time.sleep(15)
             response = requests.put(config['CHECKSYSTEM_URL'], headers={'X-Team-Token': config['CHECKSYSTEM_TOKEN']}, json=[item.flag for item in flags], timeout=5)
+            print(response.status_code)
+            print(response.text)
 
+            flags = response.json()
+            print(flags)
             if response.status_code == 200:
-                flags = response.json()
-                for flag in response:
-                    connection.execute(text("UPDATE flags SET status = :status, checksystem_response = :response WHERE flag = :flag"), {"status": FlagStatus.ACCEPTED.value if flag["status"] else FlagStatus.REJECTED.value, "flag": flag["flag"], "response": flag["msg"]})
-
+                for flag in flags:
+                    print(flag)
+                    connection.execute(text("UPDATE flags SET status = :status, checksystem_response = :response WHERE flag = :flag"), {"status": FlagStatus.ACCEPTED.value if flag["status"] else FlagStatus.REJECTED.value, "flag": flag["flag"], "response": flag["msg"]})   
                 connection.commit()
-            else:
-                print(response.text)
-
         except:
             pass
